4df484213e8a25abee81130b765ad366c104a3e3
Ivan Luis Leiva <illeiva@outlook.com>
2024-07-11 18:27:05 +0000

    Save progress: 1710.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -141,6 +141,7 @@ if [[ ! $BIKE_ID_TO_RETURN =~ ^[0-9]+$ ]]
       # update date_returned
       RETURN_BIKE_RESULT=$($PSQL "UPDATE rentals SET date_returned = NOW() WHERE rental_id=$RENTAL_ID")
       # set bike availability to true
+      SET_TO_TRUE_RESULT=$($PSQL "UPDATE bikes SET available = true WHERE bike_id = $BIKE_ID_TO_RETURN")
       # send to main menu
       fi
     fi
