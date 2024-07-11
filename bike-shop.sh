d273de3676bbf89d864dd703d57c89dd37ccf5f8
Ivan Luis Leiva <illeiva@outlook.com>
2024-07-11 15:37:52 +0000

    Save progress: 1411.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -82,7 +82,7 @@ RENT_MENU() {
         SET_TO_FALSE_RESULT=$($PSQL "update bikes set available=false where bike_id=$BIKE_ID_TO_RENT")
         # get bike info
         BIKE_INFO=$($PSQL "SELECT size, type FROM bikes where bike_id=$BIKE_ID_TO_RENT")
-        echo $BIKE_INFO
+        echo $BIKE_INFO | sed 's/ |/"/'
         # send to main menu
       fi
     fi
