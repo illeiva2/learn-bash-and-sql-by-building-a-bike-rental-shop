1c8b8f598ef85b71b79097762c76bca73e41c73d
Ivan Luis Leiva <illeiva@outlook.com>
2024-07-11 17:27:57 +0000

    Save progress: 1438.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -65,25 +65,29 @@ RENT_MENU() {
         CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$PHONE_NUMBER'")
 
         # if customer doesn't exist
-          if [[ -z $CUSTOMER_NAME ]]
-          then
-
-        # get new customer name
+        if [[ -z $CUSTOMER_NAME ]]
+        then
+          # get new customer name
           echo -e "\nWhat's your name?"
           read CUSTOMER_NAME
-        # insert new customer
-        INSERT_CUSTOMER_RESULT=$($PSQL "insert into customers(name, phone) values('$CUSTOMER_NAME', '$PHONE_NUMBER')")
+
+          # insert new customer
+          INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$PHONE_NUMBER')") 
         fi
+
         # get customer_id
-        CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$PHONE_NUMBER'")
+        CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$PHONE_NUMBER'")
+
         # insert bike rental
-        INSERT_RENTAL_RESULT=$($PSQL "insert into rentals(customer_id, bike_id) values($CUSTOMER_ID, $BIKE_ID_TO_RENT)")
+        INSERT_RENTAL_RESULT=$($PSQL "INSERT INTO rentals(customer_id, bike_id) VALUES($CUSTOMER_ID, $BIKE_ID_TO_RENT)")
+
         # set bike availability to false
-        SET_TO_FALSE_RESULT=$($PSQL "update bikes set available=false where bike_id=$BIKE_ID_TO_RENT")
+        SET_TO_FALSE_RESULT=$($PSQL "UPDATE bikes SET available = false WHERE bike_id = $BIKE_ID_TO_RENT")
+
         # get bike info
-        BIKE_INFO=$($PSQL "SELECT size, type FROM bikes where bike_id=$BIKE_ID_TO_RENT")
-        echo $BIKE_INFO | sed 's/ |/"/'
+        BIKE_INFO=$($PSQL "SELECT size, type FROM bikes WHERE bike_id = $BIKE_ID_TO_RENT")
         BIKE_INFO_FORMATTED=$(echo $BIKE_INFO | sed 's/ |/"/')
+        
         # send to main menu
         MAIN_MENU "I have put you down for the $BIKE_INFO_FORMATTED Bike, $CUSTOMER_NAME."
       fi
